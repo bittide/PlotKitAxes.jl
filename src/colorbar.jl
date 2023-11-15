91f58fb03d070654e327e908f6571188565a1500
Sanjay Lall <sanjaylall@google.com>
2023-11-14 22:34:30 -0800

    minor

--- src/colorbar.jl
+++ src/colorbar.jl
@@ -5,7 +5,7 @@ using ..PlotKitCairo: Gradient, Point, add_color_stop, destroy, linear_pattern, 
 using ..AxisDrawables: AxisDrawable
 using ..DrawAxis: drawaxis, setclipbox
 
-export colorbar
+export colorbar, categoricalcolorbar
 
 ##############################################################################
 # colorbar
@@ -27,6 +27,36 @@ function colorbar(g::Gradient; cmin = 0, cmax = 1, kw...)
     end
     rect(ad, Point(0,cmin), Point(1,cmax-cmin); fillcolor = pat)
     destroy(pat)
+    return ad
+end
+
+function categoricalcolorbar(colors, values; cmin = 0, cmax = 1, kw...)
+    ad = AxisDrawable([Point(0,cmin), Point(1,cmax)]; width = 200,
+                      axisstyle_drawxlabels = false,
+                      axisstyle_ytickhorizontaloffset = -26,
+                      yidealnumlabels = 6,
+                      axisbox_ymin = cmin,
+                      axisbox_ymax = cmax,
+                      xticksatright = true, kw...)
+    drawaxis(ad)
+    setclipbox(ad)
+    # length values = length colors + 1
+    # values lie between 0 and 1
+    # and values[1] = 0
+    vmin = values[1]
+    vmax = values[end]
+    #println((;values))
+    intx(v) = (1-v)*cmin + v*cmax
+    for i = 1:length(colors)
+        cstart = values[i]
+        if i == length(colors)
+            cend = 1
+        else
+            cend = values[i+1]
+        end
+        #println((;cstart, cend))
+        rect(ad, Point(0,intx(cstart)), Point(1, intx(cend)); fillcolor = colors[i])
+    end
     return ad
 end
 
